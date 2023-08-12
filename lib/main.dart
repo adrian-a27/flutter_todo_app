a63d5521f692385a2395cdf8b9dcc14267ed1a3b
Adrian Anaemeje <anaemeje@mit.edu>
2023-08-11 22:32:47 -0700

    Implement device theme matching

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+  MainApp({super.key});
+  var mainColor = Colors.deepPurple;
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+        title: "Todo List",
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: mainColor, brightness: Brightness.light),
+        ),
+        darkTheme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: mainColor, brightness: Brightness.dark),
+        ),
+        themeMode: ThemeMode.system,
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            print('button pressed');
+          },
+          child: Text("Hello, world!"),
         ),
       ),
     );
