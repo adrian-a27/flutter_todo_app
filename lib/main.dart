a1541292e45dce2f4a5e7db128c8b8be76edc7c6
Adrian Anaemeje <anaemeje@mit.edu>
2023-08-12 03:18:22 -0700

    Finish task page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_adaptive_scaffold/flutter_adaptive_scaffold.dart';
+import 'package:todo_app/widgets/task_group.dart';
+import './widgets/page_header.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MainApp());
@@ -6,38 +10,67 @@ void main() {
 
 class MainApp extends StatelessWidget {
   MainApp({super.key});
-  var mainColor = Colors.deepPurple;
+  var mainColor = Colors.purple;
+
   @override
   Widget build(BuildContext context) {
+    var themeLight = ThemeData(
+      useMaterial3: true,
+      fontFamily: GoogleFonts.lexendDeca().fontFamily,
+      colorScheme: ColorScheme.fromSeed(
+          seedColor: mainColor, brightness: Brightness.light),
+    );
+    var themeDark = ThemeData(
+      useMaterial3: true,
+      fontFamily: GoogleFonts.lexendDeca().fontFamily,
+      colorScheme: ColorScheme.fromSeed(
+          seedColor: mainColor, brightness: Brightness.dark),
+    );
+
+    // var flag = ThemeMode.system;
+
     return MaterialApp(
         title: "Todo List",
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: mainColor, brightness: Brightness.light),
-        ),
-        darkTheme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: mainColor, brightness: Brightness.dark),
-        ),
+        theme: themeLight,
+        darkTheme: themeDark,
         themeMode: ThemeMode.system,
-        home: HomePage());
+        home: AdaptiveScaffold(
+          destinations: [
+            NavigationDestination(
+                icon: Icon(Icons.view_agenda_outlined), label: 'Agenda'),
+            NavigationDestination(icon: Icon(Icons.list_alt), label: 'Tasks'),
+            NavigationDestination(
+                icon: Icon(Icons.calendar_today), label: 'Calendar'),
+            NavigationDestination(
+                icon: Icon(Icons.settings_outlined), label: 'Settings')
+          ],
+          selectedIndex: 1,
+          body: (_) => Container(
+              color: themeDark.colorScheme.surface,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: TasksPage(),
+              )),
+          useDrawer: false,
+        ));
   }
 }
 
-class HomePage extends StatelessWidget {
+class TasksPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            print('button pressed');
-          },
-          child: Text("Hello, world!"),
-        ),
-      ),
+    return Column(
+      children: [
+        PageHeader(text: "Tasks"),
+        TaskGroup(heading: "Today", tasks: [
+          "This is a task",
+          "This is another task",
+          "Finish the other tasks"
+        ]),
+        TaskGroup(
+            heading: "Tomorrow",
+            tasks: ["Just do the other stuff first", "Don't worry about this"])
+      ],
     );
   }
 }
