10ed2f2d79011d71a5bbd0366b4ea154f0fb6902
Adrian Anaemeje <anaemeje@mit.edu>
2023-08-20 23:56:40 -0700

    Change calls to use AuthManager

--- lib/utils/googleapi_calls.dart
+++ lib/utils/googleapi_calls.dart
@@ -1,14 +1,12 @@
-import 'package:extension_google_sign_in_as_googleapis_auth/extension_google_sign_in_as_googleapis_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:googleapis/calendar/v3.dart' as gcal;
+import 'package:todo_app/providers/auth_manager.dart';
 
 // TODO: Handle potential error for this call
-Future<List<gcal.Event>> getEvents(GoogleSignIn googleSignIn, String calendarId,
+Future<List<gcal.Event>> getEvents(AuthManager authManager, String calendarId,
     {DateTime? timeMin, DateTime? timeMax}) async {
   print("Function called");
-  var client = await googleSignIn.authenticatedClient();
 
-  var calendarApi = gcal.CalendarApi(client!);
+  var calendarApi = await authManager.calendarApi;
   var eventList = await calendarApi.events
       .list(calendarId,
           orderBy: "startTime",
