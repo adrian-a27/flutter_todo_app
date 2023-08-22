53a1e82044c3ab49b058a4a978980d54b047694a
Adrian Anaemeje <anaemeje@mit.edu>
2023-08-22 00:39:22 -0700

    Add withDate constructor and padding

--- lib/widgets/event_stream.dart
+++ lib/widgets/event_stream.dart
@@ -1,33 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'event.dart';
 
 class EventStream extends StatelessWidget {
-  final String? heading;
+  late String heading;
   final List<Event> events;
+  final DateTime? date;
+  bool scrollable;
 
-  const EventStream({super.key, required this.heading, required this.events});
+  EventStream(
+      {super.key,
+      required this.heading,
+      required this.events,
+      this.date,
+      this.scrollable = false});
+
+  EventStream.withDate(
+      {required this.date, required this.events, this.scrollable = true}) {
+    heading = date!.year == DateTime.now().year
+        ? DateFormat.MMMMd().format(date!.toLocal())
+        : DateFormat.yMMMMd('en_US').format(date!.toLocal());
+  }
+
   // TODO: Create automatic group based on date
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Expanded(
-              child: Text(heading!,
-                  style: Theme.of(context).textTheme.headlineSmall),
-            ),
-            Text(events.length.toString(),
-                style: Theme.of(context).textTheme.headlineSmall)
-          ],
-        ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: events.length,
-            itemBuilder: (BuildContext context, int index) => events[index],
-          ),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        if (scrollable) {
+          return Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(heading,
+                        style: Theme.of(context).textTheme.headlineSmall),
+                  ),
+                  Text(events.length.toString(),
+                      style: Theme.of(context).textTheme.headlineSmall)
+                ],
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: events.length,
+                  itemBuilder: (BuildContext context, int index) =>
+                      events[index],
+                ),
+              ),
+            ],
+          );
+        } else {
+          return Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Text(heading,
+                        style: Theme.of(context).textTheme.headlineSmall),
+                  ),
+                  Text(events.length.toString(),
+                      style: Theme.of(context).textTheme.headlineSmall)
+                ],
+              ),
+              for (var event in events) event
+            ],
+          );
+        }
+      }),
     );
   }
 }
