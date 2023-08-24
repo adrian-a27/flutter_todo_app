021f31f9fc2e065967b17f4bc3b83f2e8f3d39d8
Adrian Anaemeje <anaemeje@mit.edu>
2023-08-23 23:58:18 -0700

    Remove googleapi dependency

--- lib/screens/calendar_page.dart
+++ lib/screens/calendar_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:googleapis/calendar/v3.dart' as gcal;
 import 'package:provider/provider.dart';
 import '../providers/auth_manager.dart';
 import '../widgets/page_header.dart';
-import '../widgets/event.dart';
+import '../widgets/event_stream.dart';
 import '../utils/googleapi_calls.dart';
 
 class CalendarPage extends StatefulWidget {
@@ -13,7 +12,7 @@ class CalendarPage extends StatefulWidget {
 
 class _CalendarPageState extends State<CalendarPage>
     with AutomaticKeepAliveClientMixin {
-  late Future<List<gcal.Event>> _eventList;
+  late Future<List<EventStream>> _eventList;
 
   @override
   bool get wantKeepAlive => true;
@@ -22,7 +21,7 @@ class _CalendarPageState extends State<CalendarPage>
   void initState() {
     super.initState();
 
-    _eventList = getEvents(context.read<AuthManager>(), "primary",
+    _eventList = getEvents(context.read<AuthManager>(),
         timeMin: DateTime.now());
   }
 
@@ -37,7 +36,7 @@ class _CalendarPageState extends State<CalendarPage>
           child: FutureBuilder(
             future: _eventList,
             builder: (BuildContext context,
-                AsyncSnapshot<List<gcal.Event>> snapshot) {
+                AsyncSnapshot<List<EventStream>> snapshot) {
               if (snapshot.connectionState == ConnectionState.done) {
                 if (snapshot.hasError) {
                   // TODO: Put in a proper error screen
@@ -45,10 +44,11 @@ class _CalendarPageState extends State<CalendarPage>
                 }
 
                 return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                  itemBuilder: (BuildContext context, int index) =>
-                      Event.fromGoogleCalendarEvent(snapshot.data![index]),
-                );
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      snapshot.data![index].scrollable = false;
+                      return snapshot.data![index];
+                    });
               } else {
                 return Container(
                   alignment: Alignment.center,
